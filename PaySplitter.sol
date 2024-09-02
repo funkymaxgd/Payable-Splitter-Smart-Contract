ecd03a5c09ce595707aec2089579a1f3ef203a0b
funkymaxgd <70129533+funkymaxgd@users.noreply.github.com>
2024-09-01 21:05:08 -0400

    Update PaySplitter.sol
    Completed more of the functionality, including adding wallets, viewing which wallets are on the smart contract

--- PaySplitter.sol
+++ PaySplitter.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 
 // This contract should
-// store a list of addresses
-// evenly divide the receipt of ETHER to all wallet addresses on the list
-// this contract is controlled by an owner, who may or may not be receiving ETH
+// ** store a list of addresses
+// ** evenly divide the receipt of ETHER to all wallet addresses on the list
+// note: this contract is controlled by an owner, who may or may not be receiving ETH
 
 
 pragma solidity >=0.8.2 <0.9.0;
@@ -18,17 +18,33 @@ contract PaySplitter {
         address wallet;
     }
 
+    WalletList[] public walletPayees;
+
+    mapping(string => address) public walletToName;
+
+    // set contract administrator
     function createOwner() public returns (address) {
         owner = msg.sender;
 
+        return (owner);
     }
 
     // adding a person to the list of addresses
-
-
-
-    function addWalletToList() public {
+    function addWalletToList(string memory _name, address _wallet) public {
         require(msg.sender == owner, "Caller is not owner");
+        walletPayees.push(WalletList(_name, _wallet)); // add to payees list struct object
+        walletToName[_name] = _wallet; // add to mapping
+
+        // don't let wallet be added twice ****************************** must fix
+    }
+
+    function viewWallets() public view returns (address[] memory){
+        require(msg.sender == owner, "Caller is not owner");
+        address[] memory addressList = new address[](walletPayees.length);
+        for (uint i=0; i < walletPayees.length; i++) {
+            addressList[i] = walletPayees[i].wallet;
+        }
+        return (addressList);
     }
 
     // payable function which splits it between the people
